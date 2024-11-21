6538f705b8221cb725544b3ddfb34834d0428417
sassmatheus <sassmatheush@gmail.com>
2024-11-21 17:35:31 -0300

    Exceptions - delete

--- src/main/java/com/projeto/curso/services/UserService.java
+++ src/main/java/com/projeto/curso/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.projeto.curso.entities.User;
 import com.projeto.curso.repositories.UserRepository;
+import com.projeto.curso.services.exceptions.DatabaseException;
 import com.projeto.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +32,15 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException(id);
+        }
+        try {
+            repository.deleteById(id); 
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+               
     }
 
     public User update(Long id, User obj){
